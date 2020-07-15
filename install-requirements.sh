17d4fc55a5b2d6c09fd954657ce8b9732c0690b0
mZzgamer <66206932+mZzgamer@users.noreply.github.com>
2020-07-15 08:57:58 +0000

    Add files via upload

---
+++ install-requirements.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo ""
+echo "Installing requirements...."
+echo ""
+
+#PYTHON
+apt-get install python
+apt-get install python3
+apt-get install python3-pip
+#osrframework
+apt install osrframework
+
+echo ""
+echo "Done"
