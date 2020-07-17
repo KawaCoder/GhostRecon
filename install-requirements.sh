cfeb6587923c841a3cb6f7166a385cb84fcc74bc
mZzgamer <66206932+mZzgamer@users.noreply.github.com>
2020-07-17 10:50:43 +0000

    Update install-requirements.sh

--- install-requirements.sh
+++ install-requirements.sh
@@ -97,7 +97,6 @@ echo ""
 echo -e "\e[1;31m [*] MOVING DIRECTORIES\e[0m"
 echo "-------------------------------------------------------------------------------------------------------------"
 echo ""
-cd GhostRecon/
 mv -v ./Grecon /usr/bin/
 mv -v firefly /root/
 
